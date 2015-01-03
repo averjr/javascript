51479c42804b9c963d2430fe753896ac596154a1
Andrey Averkiev <averkiev.jr@gmail.com>
2015-01-03 13:34:32 +0200

    code refactored - few circles avaliable

--- bubbles.js
+++ bubbles.js
@@ -5,56 +5,69 @@
         game = new Game();    
         return;
     }
-
     
     function Game(){
-        var width = 500,
+        var 
+            width = 500,
             height = 500,
             ctx,
-            canvas;
+            canvas,
+            objects = [];
 
         this.init = function() {
             canvas = document.getElementById('bubbles');
 			ctx = canvas.getContext('2d');
 
-            var c = new Circle(60,60);
+            objects.push(new Circle(60,60), new Circle(160, 160) );
             canvas.addEventListener('click', function(e){
-                c.checkHit(e.clientX, e.clientY);
-
-            });
-//            new Circle(160,160);
+                this.checkHit(e.clientX, e.clientY);
+            }.bind(this));
+            
+            this.render();
         }
 
-        
-        function Circle(x,y){
-            var counter = 1,
-                radius = 30;
-            
-            this.render = function(){
-    	    	ctx.clearRect(0, 0, width, height);
-  	        
+        this.render = function() {
+          	ctx.clearRect(0, 0, width, height);
+            for(var i=0; i<objects.length; i++ ) {
                 ctx.beginPath();
-    	    	ctx.fillStyle = '#000';
-    	    	ctx.arc(x,y,radius,0,Math.PI*2,true);
-    		    ctx.fill();
+
+        	    ctx.fillStyle = '#000';
+        	    ctx.arc(objects[i].x,
+                        objects[i].y,
+                        objects[i].radius,
+                        0,
+                        Math.PI*2,
+                        true);
+    	    	ctx.fill();
     
-        		ctx.fillStyle = '#fff';
-		        ctx.font = 10 + 'px Helvetica';
-        		ctx.fillText(counter, x,y);
+        	    ctx.fillStyle = '#fff';
+    		    ctx.font = 10 + 'px Helvetica';
+            	ctx.fillText(objects[i].counter, 
+                             objects[i].x,
+                             objects[i].y);
             }
+        }
 
-            this.checkHit = function(cx, cy){
-                var distancesquared = (x - cx) * (x - cx) + (y - cy) * (y - cy);
+        this.checkHit = function(cx, cy) {
+            for(var i=0; i<objects.length; i++){
+                var distancesquared = (objects[i].x - cx) * (objects[i].x - cx) + (objects[i].y - cy) * (objects[i].y - cy);
 
-                if(distancesquared <= radius * radius) {
-                    counter = counter + 1;
+                if(distancesquared <= objects[i].radius * objects[i].radius) {
+                    objects[i].counter = objects[i].counter + 1;
+                 
                     this.render();
                 }
             }
-               
-            this.render(); 
-        }    
+        }
 
         this.init();
     }
+    
+    function Circle(x,y){
+        this.x = x;
+        this.y = y;
+        this.counter = 1;
+        this.radius = 30;
+    }
+     
 })();
